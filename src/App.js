106ad16bb44ba7802163e2c113c88571058a5784
Ariiie <oluwafunmilayodada2018@gmail.com>
2022-11-10 19:20:23 +0100

    added cardlist component and search-box component

--- src/App.js
+++ src/App.js
@@ -1,58 +1,71 @@
 import "./App.css";
-import { Component } from "react";
+import {Component} from "react";
+import CardList from "./components/card-list/CardList";
 
 class App extends Component {
-  constructor() {
-    super();
+	constructor() {
+		super();
 
-    this.state = {
-      monsters: [],
-    };
-  }
+		this.state = {
+			monsters: [],
+		};
+	}
 
-  componentDidMount() {
-    fetch(`https://jsonplaceholder.typicode.com/users`)
-      .then((response) => response.json())
-      .then((users) =>
-        this.setState(
-          () => {
-            return {
-              monsters: users,
-              searchField: "",
-            };
-          },
-          () => {
-            console.log(this.state);
-          }
-        )
-      );
-  }
+	componentDidMount() {
+		fetch(`https://jsonplaceholder.typicode.com/users`)
+			.then((response) => response.json())
+			.then((users) =>
+				this.setState(
+					() => {
+						return {
+							monsters: users,
+							searchField: "",
+						};
+					},
+					() => {
+						console.log(this.state);
+					}
+				)
+			);
+	}
 
-  render() {
-    const filteredMonsters = this.state.monsters.filter((monster) => {
-      return monster.name.toLocaleLowerCase().includes(this.state.searchField);
-    });
+	onSearchChange = (e) => {
+		const searchField = e.target.value.toLocaleLowerCase();
+		this.setState(() => {
+			return {searchField};
+		});
+	};
 
-    return (
-      <div className="App">
-        <input
-          type="search-box"
-          placeholder="search monsters"
-          onChange={(e) => {
-            console.log(e.target.value);
+	render() {
+		//destructuring
+		const {monsters, searchField} = this.state;
+		const {onSearchChange} = this;
 
-            const searchField = e.target.value.toLocaleLowerCase();
-            this.setState(() => {
-              return { searchField };
-            });
-          }}
-        />
-        {filteredMonsters.map((monster) => {
-          return <h1 key={monster.id}>{monster.name}</h1>;
-        })}
-      </div>
-    );
-  }
+		const filteredMonsters = monsters.filter((monster) => {
+			return monster.name.toLocaleLowerCase().includes(searchField);
+		});
+
+		return (
+			<div className="App">
+				<input
+					type="search-box"
+					placeholder="search monsters"
+					onChange={onSearchChange}
+				/>
+				{/* {filteredMonsters.map((monster) => {
+					return <h1 key={monster.id}>{monster.name}</h1>;
+				})} */}
+				<CardList monsters={filteredMonsters} />
+			</div>
+		);
+	}
 }
 
 export default App;
+
+//anonymouse function is a function that is not stored anywhere in a variable
+//creating methods in renders are for optimization
+// a component governs a piece of the ui - ties together visual representation and functional representation of the ui
+
+//props are properties,
+//"this" actually references the actual class component itself
